d757d4728f3717449c2f8d6a0761fb29a876bea7
abdul8377 <abdulalfonso77@gmail.com>
2026-08-03 11:49:35 -0500

    first commit

---
+++ src/main/java/com/example/backendappcatalogo/BackendAppCatalogoApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendappcatalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAppCatalogoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendAppCatalogoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendappcatalogo/BackendAppCatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendappcatalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAppCatalogoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
