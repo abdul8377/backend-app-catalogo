330ef56400f2956568b8ed5c8c68a08f78bf94b2
abdul alfonso <104173085+abdul8377@users.noreply.github.com>
2026-08-04 22:57:03 -0500

    agrega archivo de imagenes a importaciones

---
+++ src/main/resources/db/migration/V10__product_import_image_archive.sql
@@ -0,0 +1,6 @@
+ALTER TABLE product_imports
+    ADD COLUMN image_archive_name VARCHAR(255) NULL,
+    ADD COLUMN image_archive_hash CHAR(64) NULL,
+    ADD COLUMN image_archive_storage_key VARCHAR(500) NULL;
+
+CREATE INDEX idx_product_import_image_hash ON product_imports(image_archive_hash);
